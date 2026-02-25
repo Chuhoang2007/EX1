239d0a2f40f4f29e0df2af438db3ace11cbdad0a
Chuhoang2007 <25023476@vnu.edu.vn>
2026-02-25 15:56:52 +0700

    update lan 2

--- src/EX1.java
+++ src/EX1.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class EX1 {
 
     public static void main(String[] args) {
         String name="Hoang dep try";
         System.out.println("Toi ten la "+name);
+        Scanner sc =  new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a%2==0){
+            System.out.println(a+" la so chan");
+        }
+        else{
+            System.out.println(a+" la so le");
+        }
 
     }
 }
