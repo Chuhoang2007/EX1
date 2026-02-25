93ca7aae158d8dc41b1cc615ac881b4fc4ee4638
Chuhoang2007 <25023476@vnu.edu.vn>
2026-02-25 21:27:13 +0700

    update lan 3

--- src/EX1.java
+++ src/EX1.java
@@ -12,6 +12,30 @@ public class EX1 {
         else{
             System.out.println(a+" la so le");
         }
-
+        long b=sc.nextLong();
+        if((b%400==0) || ((b%4==0) && (b%100!=0))){
+            System.out.println(b+" la nam nhuan");
+        }
+        else{
+            System.out.println(b+" khong phai nam nhuan");
+        }
+        float c= sc.nextFloat();
+        float GPA;
+        if(c>=9.0){
+            GPA = 4.0F;
+        }
+        else if(c<9.0 && c>=8.5) {
+            GPA = 3.7F;
+        }
+        else if(c>=8.0 && c<8.5){
+            GPA = 3.5F;
+        }
+        else if(c>=7.5 && c<8.0){
+            GPA = 3.0F;
+        }
+        else{
+            GPA=2.5F;
+        }
+        System.out.println("GPA cua "+name+" la: "+GPA);
     }
 }
